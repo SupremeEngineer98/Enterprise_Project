ee1efc09a69b850774e5fa080dade3a0102e64ff
peng1434 <wp1434780366@gmail.com>
2026-02-09 00:12:12 +0200

    add file for home page
    add html,css and js file , the "img1.png" is just an example,can be replaced with any else image

---
+++ Home page/script.js
@@ -0,0 +1,16 @@
+const images = document.querySelectorAll('#slider img');
+let index = 0;
+
+function nextImage() {
+  // 1. remove the "active"
+  images[index].classList.remove('active');
+  
+  // 2. loop for all pictures
+  index = (index + 1) % images.length;
+  
+  // 3. let the next picture be "active"
+  images[index].classList.add('active');
+}
+
+// automatic change every 3 second
+setInterval(nextImage, 3000);
