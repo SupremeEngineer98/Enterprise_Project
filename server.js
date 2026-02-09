d6ae80a6441f70ed91bcd38ff7948ebee929cff1
wisewin <gmavropouloscoding@gmail.com>
2026-02-09 14:49:14 +0200

    Just created the get carousel images endpoint

--- server.js
+++ server.js
@@ -467,6 +467,44 @@ app.put('/winwise/update/projects/:id',(req,res)=>{
     });
 
 
+    //endpoint which allows users to retrieve carousel images!
+    app.get('/carousel/images',(req,res)=>{
+
+        //creating the sql query!
+        let sql = 'SELECT * FROM carousel';
+
+        //executing the query!
+        DB.all(sql,[],function(err,rows){
+              //returning an error message if server does not respond
+            if(err)
+            {
+            return res.status(500).json({message:`Server does not respond:${err.message}`});
+            }
+
+            //returning an error message if there isn't at least one images inside the DB!
+            if(!rows)
+            {
+                return res.status(404).json({message:`Images could not be found`});
+            }
+
+            //if images exist!
+            let data = {images:[]};
+
+            //pushing data to the array via forEach loop!
+            rows.forEach((row)=>{
+                data.images.push({
+                    id:row.id,
+                    image:row.image_url,
+                    title:row.title
+                });
+            });
+
+            //returning data in json format!
+            return res.status(200).json(data);
+        })
+    });
+
+
 //method to configure port!
 app.listen(port, (err)=>{
     //returning an error message if sth goes wrong with the port!
